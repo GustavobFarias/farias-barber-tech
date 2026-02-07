9c1610d48249ffc30ba7d6019d8eb30ac31f3d49
Gustavo <gustavoburgerf@gmail.com>
2026-02-06 22:20:39 -0300

    chore: add git hooks setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from "./_components/ui/button";
+import { Button } from "./_components/ui/button"
 
 function Home() {
   return <Button>Teste</Button>
 }
 
-export default Home;
+export default Home
