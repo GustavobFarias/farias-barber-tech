626786612ed4878dae4ace22d4f2de22f8aed0ef
Gustavo <gustavoburgerf@gmail.com>
2026-02-07 14:10:04 -0300

    feat: add Header components and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/Header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 function Home() {
-  return <Button>Teste</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2>Olá, Gustavo!</h2>
+        <p>Sábado, 07 de Fevereiro</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner.png"
+            alt="Agende nos melhores com FSN Barber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
