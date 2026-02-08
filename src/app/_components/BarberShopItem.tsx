3c7553ac1dbb2710e8e7683931ed96b795ef282b
Gustavo <gustavoburgerf@gmail.com>
2026-02-08 16:18:27 -0300

    feat: add initial barbershop page

--- src/app/_components/BarberShopItem.tsx
+++ src/app/_components/BarberShopItem.tsx
@@ -4,6 +4,7 @@ import { Button } from "./ui/button"
 import { Card, CardContent } from "./ui/card"
 import Image from "next/image"
 import { Badge } from "./ui/badge"
+import Link from "next/link"
 
 interface BarberShopItemprops {
   barberShop: BarberShop
@@ -29,12 +30,11 @@ const BarberShopId = ({ barberShop }: BarberShopItemprops) => {
             <p>5,0</p>
           </Badge>
         </div>
-
         <div className="px-1 py-3">
           <h3 className="truncate font-semibold">{barberShop.name}</h3>
           <p className="truncate text-sm text-gray-400">{barberShop.address}</p>
-          <Button variant="secondary" className="mt-3 w-full">
-            Reservar
+          <Button variant="secondary" className="mt-3 w-full" asChild>
+            <Link href={`/barbershops/${barberShop.id}`}>Reservar</Link>
           </Button>
         </div>
       </CardContent>
